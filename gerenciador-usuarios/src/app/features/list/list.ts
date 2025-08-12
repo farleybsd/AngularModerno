e80023eb0ae020f1dcf5ed556bdb7ecddec65a0e
farleybsd <farley.t.i@hotmail.com>
2025-08-12 19:49:39 -0300

    Observer

--- gerenciador-usuarios/src/app/features/list/list.ts
+++ gerenciador-usuarios/src/app/features/list/list.ts
@@ -1,46 +1,68 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 
 import { SearchInput } from './components/search-input/search-input';
 import { UsersList } from './components/users-list/users-list';
 import { User } from '../../shared/interface/usuario';
 import { Users } from '../../shared/services/users';
 
+import { take } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+
 @Component({
-    selector: 'app-list',
-    template: `
-@if (isLoading()) {
-  <p>Carregando...</p>
-} @else {
+  selector: 'app-list',
+  template: `
   <app-search-input [(search)]="search"/>
-  <app-users-list [users]="filteredUsers()" (remover)="remove($event)"/>
+
+  @if (isLoading()) {
+    <p>Carregando...</p>
+  } @else {
+  <app-users-list [users]="users()" (remover)="remove($event)"/>
 }`,
-    imports: [SearchInput, UsersList]
+  imports: [SearchInput, UsersList]
 })
 
 export class List implements OnInit {
-    constructor() { }
 
-   userService = inject(Users);
-
-   users = signal<User[]>([]);
-   search = signal('');
-   searchInLowrCase= computed(() => this.search().toLocaleLowerCase());
-   isLoading = signal(true);
-  
+  constructor() {
+    // Efect
+    effect(() => {
+      this.isLoading.set(true);
+      this.getUsers();
+    });
+  }
+  DestroyRef = inject(DestroyRef);
+  userService = inject(Users);
+  users = signal<User[]>([]);
+  search = signal('');
+  isLoading = signal(true);
+  //searchInLowrCase= computed(() => this.search().toLocaleLowerCase());
   // Toda fez que o sinal emitir um sinal essa funcao computed e executada
   // e o valor atualizado
-  filteredUsers = computed(() => {
-   return this.users().filter(user => user.name.toLocaleLowerCase()
-                                          .includes(this.searchInLowrCase()));
-  });
+  // filteredUsers = computed(() => {
+  //  return this.users().filter(user => user.name.toLocaleLowerCase()
+  //                                         .includes(this.searchInLowrCase()));
+  // });
 
   ngOnInit(): void {
-    this.userService.getAll().subscribe(users => {
+    this.getUsers();
+  }
+
+  remove({ id }: User) {
+
+    this.userService.delete(id).subscribe(() => {
+      this.users.update(users => users.filter(u => u.id != id));
+    });
+  }
+
+  private getUsers() {
+    this.userService.getAll(this.search())
+                    .pipe(
+                      takeUntilDestroyed(this.DestroyRef), // Destrói o efeito quando o componente é destruído
+                      take(1) // Pega apenas o primeiro valor emitido
+                    )
+                    .subscribe(users => {
       this.users.set(users);
       this.isLoading.set(false);
     });
   }
-   remove({id} :User){
-    this.users.update(users => users.filter(u => u.id != id));
-   }
 }
--- gerenciador-usuarios/src/app/shared/services/users.ts
+++ gerenciador-usuarios/src/app/shared/services/users.ts
@@ -1,19 +1,34 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { delay, of } from 'rxjs';
 import { User, UserPayload } from '../interface/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Users {
+
   httpClient = inject(HttpClient)
-  getAll(){
-    return this.httpClient.get<User[]>('http://localhost:3000/users');
+
+  getAll(search?: string) {
+
+    let httpParams = new HttpParams();
+
+    if (search) {
+      httpParams = httpParams.append('q', search);
+    }
+   
+    return this.httpClient.get<User[]>('http://localhost:3000/users',{
+      params: httpParams
+    });
+    
   }
 
   Post(paylod:UserPayload){
     return this.httpClient.post<User[]>('http://localhost:3000/users',paylod);
+  }
+
+  delete(id:number){
+    return this.httpClient.delete<{}>(`http://localhost:3000/users/${id}`);
   }
   
 }
