2d42151080b2d24ba0bc1f30d0655c5baf21993d
farleybsd <farley.t.i@hotmail.com>
2025-08-27 13:46:49 -0300

    Mostrando mensagem quando não há transações

--- gerenciador-financeiro/src/app/features/home/home.ts
+++ gerenciador-financeiro/src/app/features/home/home.ts
@@ -3,18 +3,19 @@ import { Balance } from './components/balance/balance';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
 import { TransactionType } from '../../shared/transaction/enums/transaction-type';
+import { NoTransactions } from './components/no-transactions/no-transactions';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance,TransactionItem],
+  imports: [Balance,TransactionItem,NoTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
    transactions = signal<Transaction[]>( [
-     {title:'Salario', value: 100, type: TransactionType.INCOME },
-     {title:'Va', value: 50, type: TransactionType.INCOME },
-     {title:'Aluguel', value: 150, type: TransactionType.OUTCOME },
+    //  {title:'Salario', value: 100, type: TransactionType.INCOME },
+    //  {title:'Va', value: 50, type: TransactionType.INCOME },
+    //  {title:'Aluguel', value: 150, type: TransactionType.OUTCOME },
     
    ]);
 }
