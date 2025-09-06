285e568125b4d6f420d9d824d202f6abc3dfcecf
farleybsd <farley.t.i@hotmail.com>
2025-09-06 13:48:41 -0300

    Constuindo formulário para adicionar transação

--- gerenciador-financeiro/src/app/features/home/pages/create/create.component.ts
+++ gerenciador-financeiro/src/app/features/home/pages/create/create.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
 
+  form = new FormGroup({
+    title: new FormControl('',{validators: [Validators.required]}),
+  });
 }
