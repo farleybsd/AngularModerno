e905c861a298c35ec33955736d2b706e78699949
farleybsd <farley.t.i@hotmail.com>
2025-09-11 14:25:53 -0300

    Adicionando button-toggle ao formulário de transação

--- gerenciador-financeiro/src/app/features/home/pages/create/create.component.ts
+++ gerenciador-financeiro/src/app/features/home/pages/create/create.component.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { TransactionType } from '../../../../shared/transaction/enums/transaction-type';
 
 
 @Component({
   selector: 'app-create',
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButton,MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButton,MatButtonModule,MatButtonToggleModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
-
+  readonly transactionType = TransactionType;
+  
   form = new FormGroup({
+    type: new FormControl('',{validators: [Validators.required]}),
     title: new FormControl('',{validators: [Validators.required]}),
     value: new FormControl('',{validators: [Validators.required]})
   });
