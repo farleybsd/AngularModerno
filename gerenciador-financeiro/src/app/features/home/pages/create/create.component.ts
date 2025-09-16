18e86ae0c3e45b532d80dc677fcd31b9d98aa1b5
farleybsd <farley.t.i@hotmail.com>
2025-09-15 21:02:46 -0300

    Utilizando withComponentInputBinding para receber dados do resolver

--- gerenciador-financeiro/src/app/features/home/pages/create/create.component.ts
+++ gerenciador-financeiro/src/app/features/home/pages/create/create.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
@@ -23,41 +23,43 @@ import { FeddbackServiceTsService } from '../../../../shared/feedback/services/f
 })
 export class CreateComponent {
 
-  private activatedRoute = inject(ActivatedRoute);
+  //private activatedRoute = inject(ActivatedRoute);
   private transactionService = inject(TransactionService);
   private feddbackServiceTsService = inject(FeddbackServiceTsService);
   private router = inject(Router);
   private _snackBar = inject(MatSnackBar);
-
+  transaction = input<Transaction>();
   readonly transactionType = TransactionType;
 
-  get transaction(): Transaction {
-    return this.activatedRoute.snapshot.data['transaction'];
-  }
+  // get transaction(): Transaction {
+  //   return this.activatedRoute.snapshot.data['transaction'];
+  // }
 
-  get isEdit(): boolean {
-    return Boolean(this.transaction);
-  }
+  isEdit = computed(() => {
+    return Boolean(this.transaction());
+  })
 
-  form = new FormGroup({
-    type: new FormControl(this.transaction?.type ?? '', { validators: [Validators.required] }),
-    title: new FormControl(this.transaction?.title ?? '', { validators: [Validators.required] }),
-    value: new FormControl(this.transaction?.value ?? 0, { validators: [Validators.required] })
-  });
+  form = computed(() => 
+    new FormGroup({
+      type: new FormControl(this.transaction()?.type ?? '', { validators: [Validators.required] }),
+      title: new FormControl(this.transaction()?.title ?? '', { validators: [Validators.required] }),
+      value: new FormControl(this.transaction()?.value ?? 0, { validators: [Validators.required] })
+    })
+  );  
 
   submint(): void {
-    if (this.form.invalid) {
+    if (this.form().invalid) {
       return;
     }
 
     const payload: TransactionPayload = {
-      title: this.form.value.title as string,
-      type: this.form.value.type as TransactionType,
-      value: this.form.value.value as number
+      title: this.form().value.title as string,
+      type: this.form().value.type as TransactionType,
+      value: this.form().value.value as number
     };
 
-    if (this.isEdit) {
-      this.transactionService.put(this.transaction.id,payload).subscribe({
+    if (this.isEdit()) {
+      this.transactionService.put(this.transaction()!.id, payload).subscribe({
         next: () => {
           this.feddbackServiceTsService.success('Transacao Alterada Com Sucesso');
           this.router.navigate(['/']);
