240a179998920e919f8439db3e3090429b9701e7
farleybsd <farley.t.i@hotmail.com>
2025-09-17 20:14:19 -0300

    Usando MatDialog para confirmar ação de remover transação

--- gerenciador-financeiro/src/app/features/home/home.ts
+++ gerenciador-financeiro/src/app/features/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
 import { TransactionItem } from './components/transaction-item/transaction-item';
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
@@ -7,7 +7,26 @@ import { TransactionService } from '../../shared/transaction/services/transactio
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterLink } from '@angular/router';
 import { FeddbackServiceTsService } from '../../shared/feedback/services/feddback.service.ts.service';
+import { MatDialog, MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef, MatDialogTitle } from '@angular/material/dialog';
+import { filter } from 'rxjs';
 
+@Component({
+  selector: 'dialog-animations-example-dialog',
+  template: `<h2 mat-dialog-title>Deletar Transacao</h2>
+<mat-dialog-content>
+  Voce tem certeza que deseja deletar essa transacao?
+</mat-dialog-content>
+<mat-dialog-actions>
+  <button matButton [mat-dialog-close]="false">Nao</button>
+  <button matButton [mat-dialog-close]="true" cdkFocusInitial>Sim</button>
+</mat-dialog-actions>
+`,
+  imports: [MatButtonModule, MatDialogActions, MatDialogClose, MatDialogTitle, MatDialogContent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogAnimationsExampleDialog {
+  readonly dialogRef = inject(MatDialogRef<DialogAnimationsExampleDialog>);
+}
 
 @Component({
   selector: 'app-home',
@@ -21,6 +40,7 @@ export class Home implements OnInit {
   private transactionService = inject(TransactionService);
   private feedback = inject(FeddbackServiceTsService);
   private router = inject(Router);
+  private dialog = inject(MatDialog);
 
   transactions = signal<Transaction[]>([]);
 
@@ -33,13 +53,17 @@ export class Home implements OnInit {
   }
 
   remove(transaction: Transaction) {
-    this.transactionService.delete(transaction.id).subscribe({
-      next: () => {
-        this.removeTransactionFromArray(transaction);
-        this.feedback.success('Transaction removed successfully👌!');
-      },
-    });
-
+    this.dialog.open(DialogAnimationsExampleDialog, {
+    }).afterClosed()
+      .pipe(filter((result: boolean) => result === true))
+      .subscribe(() => {  // Removemos o parâmetro result pois já filtramos com pipe
+        this.transactionService.delete(transaction.id).subscribe({
+          next: () => {
+            this.removeTransactionFromArray(transaction);
+            this.feedback.success('Transaction removed successfully👌!');
+          }
+        });
+      });
 
   }
 
