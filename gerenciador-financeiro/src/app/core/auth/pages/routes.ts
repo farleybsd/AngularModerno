3e21ed44ec97594908de0d993ee560231b0f6839
farleybsd <farley.ti@hotmail.com>
2025-09-29 15:03:53 -0300

    Criando componente de layout para autenticação
    Criando componente de layout para autenticação

--- gerenciador-financeiro/src/app/core/auth/pages/routes.ts
+++ gerenciador-financeiro/src/app/core/auth/pages/routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
+import { LayoutComponent } from "../components/layout/layout.component";
 
-export default [{
-    path: 'login',
-    component: LoginComponent,
-}] as Routes;
+export default [
+    {
+        path: '',
+        component: LayoutComponent,
+        children: [{
+            path: 'login',
+            component: LoginComponent,
+        }]
+    }] as Routes;
