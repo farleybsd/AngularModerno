9f84a7864c173a0519ad5937875f793cb2b54c0d
farleybsd <farley.t.i@hotmail.com>
2025-09-29 19:51:16 -0300

    Adicionando ação após autenticação

--- gerenciador-financeiro/src/app/core/auth/pages/login/login.component.ts
+++ gerenciador-financeiro/src/app/core/auth/pages/login/login.component.ts
@@ -5,18 +5,22 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input";
 import { AuthService } from '../../service/auth.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { UserCredentials } from '../../interfaces/user-credentials';
 
 @Component({
   selector: 'app-login',
-  imports: [MatCardModule,MatFormFieldModule, MatInputModule, FormsModule,
-            ReactiveFormsModule, MatButton, MatButtonModule,
-],
+  imports: [MatCardModule, MatFormFieldModule, MatInputModule, FormsModule,
+    ReactiveFormsModule, MatButton, MatButtonModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
   authService = inject(AuthService);
+  router = inject(Router);
 
   form = new FormGroup({
     user: new FormControl('', [Validators.required]),
@@ -25,20 +29,25 @@ export class LoginComponent {
 
   submint() {
 
-    if(this.form.invalid) {
+    if (this.form.invalid) {
       return;
     }
 
-    const payload = {
+    const payload : UserCredentials = {
       user: this.form.value.user as string,
-      password: this.form.value.password  as string
+      password: this.form.value.password as string
     }
     this.authService.login(payload).subscribe({
       next: (res) => {
         console.log(res);
+        this.router.navigate(['']);
       },
-      error: (err) => {
-        console.log(err);
+      error: (err: HttpErrorResponse) => {
+        if (err.status === 401) {
+          this.form.setErrors({
+            wrongCredentials: true
+          });
+        }
       }
     });
   }
