2aa4c971a8bfe66285f0f9519084baadf72d0fde
farleybsd <farley.t.i@hotmail.com>
2025-10-27 15:51:56 -0300

    Isolando renderização de itens da sidenav em um componente

--- gerenciador-financeiro/src/app/core/layout/layout.ts
+++ gerenciador-financeiro/src/app/core/layout/layout.ts
@@ -1,8 +1,8 @@
 import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
 
 @Component({
   selector: 'app-layout',
@@ -10,18 +10,11 @@ import { MatListModule } from '@angular/material/list';
     Header,
     RouterOutlet,
     MatSidenavModule,
-    MatListModule,
-    RouterLink,
-    RouterLinkActive
+    SidenavItemsComponent
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.scss'
 })
 export class Layout {
-  links =  signal([
-    {
-      label:'Home',
-      url:'/'
-    }
-  ])
+  
 }
