e5e084fb09370e48e28f92c80ef55dc47aeefbfc
farleybsd <farley.t.i@hotmail.com>
2025-10-28 20:52:22 -0300

    Controlando visibilidade da Sidenav (Parte 2)

--- gerenciador-financeiro/src/app/core/layout/components/header/header.ts
+++ gerenciador-financeiro/src/app/core/layout/components/header/header.ts
@@ -1,14 +1,24 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { LogoutFacadeService } from '../../../auth/facades/logout-facade.service';
-import { Router } from '@angular/router';
-import { LoggedInUserStoreService } from '../../../auth/stores/logged-in-user-store.service';
+import { MatIconModule } from '@angular/material/icon';
+import { SidenavVisibilityStore } from '@core/layout/stores/sidenav-visibility.store';
+import { MobileLayoutService } from '@core/layout/services/mobile-layout.service';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule,MatButtonModule],
+  imports: [MatToolbarModule,MatButtonModule,MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
-export class Header {}
+export class Header {
+
+  private readonly sidenavVisibilityStore = inject(SidenavVisibilityStore);
+  private readonly mobileLayoutService = inject(MobileLayoutService);
+
+  isMobile = this.mobileLayoutService.isMobile();
+
+  toogleVisibility(){
+    this.sidenavVisibilityStore.toogle();
+  }
+}
--- gerenciador-financeiro/src/app/core/layout/components/sidenav/sidenav.component.ts
+++ gerenciador-financeiro/src/app/core/layout/components/sidenav/sidenav.component.ts
@@ -2,6 +2,7 @@ import { Component, computed, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
 import { MobileLayoutService } from '@core/layout/services/mobile-layout.service';
+import { SidenavVisibilityStore } from '@core/layout/stores/sidenav-visibility.store';
 
 
 @Component({
@@ -16,8 +17,16 @@ import { MobileLayoutService } from '@core/layout/services/mobile-layout.service
 export class SidenavComponent {
 
   private readonly mobileLayoutService = inject(MobileLayoutService);
+  private readonly sidenavVisibilityStore = inject(SidenavVisibilityStore);
 
   isMobile = this.mobileLayoutService.isMobile();
   sidenavMode = computed(() => this.isMobile() ? 'over' : 'side');
+  isSideNavOpened = computed(() => {
+
+    if(!this.isMobile()){
+      return true;
+    }
+    return this.sidenavVisibilityStore.isVisible();
+  });
 
 }
