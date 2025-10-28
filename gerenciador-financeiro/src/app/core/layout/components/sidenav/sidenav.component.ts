aedcdb35c4de9fb49e395fdc4d26fe8abf7ddf2d
farleybsd <farley.t.i@hotmail.com>
2025-10-28 20:22:21 -0300

    Mudando como Sidenav aparece conforme media-query da página

--- gerenciador-financeiro/src/app/core/layout/components/sidenav/sidenav.component.ts
+++ gerenciador-financeiro/src/app/core/layout/components/sidenav/sidenav.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
+import { MobileLayoutService } from '@core/layout/services/mobile-layout.service';
 
 
 @Component({
@@ -12,4 +13,11 @@ import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
-export class SidenavComponent {}
+export class SidenavComponent {
+
+  private readonly mobileLayoutService = inject(MobileLayoutService);
+
+  isMobile = this.mobileLayoutService.isMobile();
+  sidenavMode = computed(() => this.isMobile() ? 'over' : 'side');
+
+}
---
+++ gerenciador-financeiro/src/app/core/layout/services/mobile-layout.service.ts
@@ -0,0 +1,23 @@
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { inject, Injectable } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MobileLayoutService {
+
+  private readonly breackPointObserver = inject(BreakpointObserver);
+
+  isMobile() {
+
+    const matches = this.breackPointObserver.observe('(max-width: 1280px)')
+      .pipe(
+        map(state => state.matches)
+      )
+
+   return toSignal(matches,{ requireSync: true });
+  }
+
+}
